fc92b25b2a926c291e9081dc48edb46ac0c9de7b
p33kaboo <80482121+p33kaboo@users.noreply.github.com>
2021-03-15 14:43:50 +0300

    Update choice_buttons.py

--- keyboards/inline/choice_buttons.py
+++ keyboards/inline/choice_buttons.py
@@ -3,17 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import URL_APPLES, URL_PEAR
 from keyboards.inline.callback_datas import buy_callback
 
-# choice = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [
-#             InlineKeyboardButton(text="Купить грушу", callback_data=buy_callback.new(item_name="pear", quantity=1)),
-#             InlineKeyboardButton(text="Купить яблоки", callback_data="buy:apple:5")
-#         ],
-#         [
-#             InlineKeyboardButton(text="Отмена", callback_data="")
-#         ]
-#     ]
-# )
 
 choice = InlineKeyboardMarkup(row_width=2)
 
